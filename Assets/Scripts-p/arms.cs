c0db2f01319883d38539f1bee1b09b087b3aedab
Daniela Di Canio <61799620+DanielaDC@users.noreply.github.com>
2021-04-20 00:01:40 -0500

    Add files via upload

---
+++ Assets/Scripts-p/arms.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class arms : MonoBehaviour
+{
+    public static bool startTotalStopwatch;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+    public void OnSelectRight(Zinnia.Pointer.ObjectPointer.EventData data)
+    {
+
+        GameObject g;
+        String n;
+        if (data != null)
+        {
+            g = data.CollisionData.transform.gameObject;
+            n = g.name;
+            if (String.Equals(n, "Easy"))
+            {
+                startTotalStopwatch = true;
+                SceneManager.LoadScene("easyArm");
+            }
+            else if (String.Equals(n, "Hard"))
+            {
+                SceneManager.LoadScene("hardArm");
+            }
+            else if (String.Equals(n, "Back"))
+            {
+                SceneManager.LoadScene("2-bodypartsMenu");
+            }
+        }
+    }
+
+    public void quit()
+    {
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+    }
+}
---
+++ Assets/Scripts-p/start.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class start : MonoBehaviour
+{
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+    public void OnSelectRight(Zinnia.Pointer.ObjectPointer.EventData data)
+    {
+
+        GameObject g;
+        String n;
+        if (data != null)
+        {
+            g = data.CollisionData.transform.gameObject;
+            n = g.name;
+            if (String.Equals(n, "Start"))
+            {
+                SceneManager.LoadScene("1-menuitems");
+            }
+        }
+    }
+
+    public void quit()
+    {
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+    }
+}
